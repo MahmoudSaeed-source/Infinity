aece9e79002ef08941e3a930278acf07c8b4df68
MAHMOUDSAEED <MahmoudSaid.wd@gmail.com>
2024-02-29 02:37:55 +0200

    destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 
-import './App.scss'
+import './App.scss';
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,Route
+  RouterProvider,
 } from 'react-router-dom'
 import Home from "./pages/ecommercy/Home";
-  import Home_Dashboard from './pages/dashboard/Home_Dashboard'
+  import Home_Dashboard from './pages/dashboard/home/Home_Dashboard'
+  import Users from './pages/dashboard/users/Users';
+  import Products from './pages/dashboard/products/Products';
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/">
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="/dashboard">
-          <Route index element={<Home_Dashboard />} />
-        </Route>
-      </>
-    )
-  );
+  
+  const router = createBrowserRouter([
+    {
+      path: "/dashboard",
+      element: <Home_Dashboard />,
+      children: [
+      {
+          path: '/dashboard/users',
+          element:<Users/>
+        },
+        {
+          path: '/dashboard/products',
+          element:<Products/>
+        }
+
+      ]
+    },
+
+    {
+      path: '/',
+      element:<Home/>
+    }
+
+  ]);
   return (
     <RouterProvider router={router}>
       <main className="app">
-        
       </main>
     </RouterProvider>
   );
--- src/pages/dashboard/Home_Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-
-const Home_Dashboard = () => {
-  return (
-    <h2>Home_Dashboard</h2>
-  )
-}
-
-export default Home_Dashboard
