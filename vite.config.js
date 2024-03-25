3b3eb3c2eb542c66e6319de1a18c34b360aeb4a1
MAHMOUDSAEED <MahmoudSaid.wd@gmail.com>
2024-03-25 03:25:58 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [legacy()],
+  plugins: [react()],
+  base:'/infinity/'
 });
